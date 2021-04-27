cfca0d72bdde1656b2e03b5b8426173959801050
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-04-27 14:43:11 +0200

    Update Latest.jsx

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -3,43 +3,53 @@ import React from "react"
 import { Jumbotron, Container, Modal, Button } from "react-bootstrap"
 import fantasy from "../data/fantasy.json"
 
-const Latest = () => {
-    const [modalShow, setModalShow] = React.useState(false)
+class Latest extends React.Component {
+    state = {
+        modalShow: false,
+        currentBook: fantasy[0]
+    }
 
-    return (
-        <>
-            <Jumbotron fluid>
-                <Container>
-                    <h1 className="pb-3">Latest Releases:</h1>
-                    {fantasy.map(book => (
-                        <img
-                            key={book.asin}
-                            className="m-1"
-                            src={book.img}
-                            alt="Book Cover"
-                            height="120px"
-                            onClick={() => {
-                                setModalShow(true)
-                            }}
-                        />
-                    ))}
-                </Container>
-            </Jumbotron>
+    render() {
+        return (
+            <>
+                <Jumbotron fluid>
+                    <Container>
+                        <h1 className="pb-3">Latest Releases:</h1>
+                        {fantasy.map(book => (
+                            <img
+                                key={book.asin}
+                                className="m-1"
+                                src={book.img}
+                                alt="Book Cover"
+                                height="120px"
+                                onClick={() => {
+                                    this.setState({ currentBook: { ...book } })
+                                    console.log(this.state.currentBook)
+                                    this.setState({ modalShow: true })
+                                }}
+                            />
+                        ))}
+                    </Container>
+                </Jumbotron>
 
-            <LatestModal show={modalShow} onHide={() => setModalShow(false)} />
-        </>
-    )
+                <LatestModal book={this.state.currentBook} show={this.state.modalShow} onHide={() => this.setState({ modalShow: false })} />
+            </>
+        )
+    }
 }
 
 const LatestModal = props => {
+    console.log(props.book)
     return (
         <Modal {...props} size="lg" aria-labelledby="latestModal" centered>
             <Modal.Header closeButton>
-                <Modal.Title id="latestModal">{props.asin}</Modal.Title>
+                <Modal.Title className="w-100 text-center" id="latestModal">
+                    {props.book.asin}
+                </Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                <h4>{props.title}</h4>
-                <img fluid src={props.img} alt={props.asin} />
+            <Modal.Body className="d-flex flex-column">
+                <h4 className="text-center">{props.book.title}</h4>
+                <img className="img-fluid" src={props.book.img} alt={props.book.asin} />
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={props.onHide}>Close</Button>
